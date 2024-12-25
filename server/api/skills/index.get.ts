81b42b11dad486a7c84e885ccd566b7f0207c6ba
Clayton Chew <30117799+claytonchew@users.noreply.github.com>
2024-12-25 22:30:36 +0800

    chore: fetch skills data from json instead of database

--- server/api/skills/index.get.ts
+++ server/api/skills/index.get.ts
@@ -1,5 +1,6 @@
-import { skillService } from "~~/server/services/database/SkillService";
+import skills from "~~/database/seed/data/skills.json";
 
 export default defineEventHandler(async () => {
-  return await skillService.getAll();
+  // To save on database queries, we can host the skills in json file and read from it instead
+  return skills.map((skill) => skill.name);
 });
