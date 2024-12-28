8147415c77b6970c26765d034ac44d4addba0b3a
Clayton Chew <30117799+claytonchew@users.noreply.github.com>
2024-12-29 01:35:44 +0800

    chore: change "your" to "my"

--- app/pages/me/projects.vue
+++ app/pages/me/projects.vue
@@ -4,7 +4,7 @@
       <UContainer>
         <div class="flex flex-wrap justify-between gap-4">
           <div>
-            <h1 class="text-lg font-semibold sm:text-xl">Your Projects</h1>
+            <h1 class="text-lg font-semibold sm:text-xl">My Projects</h1>
             <span
               class="mt-1 block text-sm text-gray-600 sm:text-base dark:text-gray-300">
               Projects added by you are listed here.
